12f1b45d0cc372393a5a0bef8ac3233c4770a2a2
Bain <alexjbain@yahoo.com>
2021-10-02 08:12:30 -0400

    Added C level stuff

--- src/p2-parser.c
+++ src/p2-parser.c
@@ -2,7 +2,7 @@
  * @file p2-parser.c
  * @brief Compiler phase 2: parser
  * Team Lima: Alice Robertson and Alexander Bain
- */ 
+ */
 
 #include "p2-parser.h"
 
@@ -16,9 +16,10 @@
  * @param input Token queue to examine
  * @returns Source line
  */
-int get_next_token_line (TokenQueue* input)
+int get_next_token_line(TokenQueue *input)
 {
-    if (TokenQueue_is_empty(input)) {
+    if (TokenQueue_is_empty(input))
+    {
         Error_throw_printf("Unexpected end of input\n");
     }
     return TokenQueue_peek(input)->line;
@@ -34,15 +35,17 @@ int get_next_token_line (TokenQueue* input)
  * @param type Expected type of next token
  * @param text Expected text of next token
  */
-void match_and_discard_next_token (TokenQueue* input, TokenType type, const char* text)
+void match_and_discard_next_token(TokenQueue *input, TokenType type, const char *text)
 {
-    if (TokenQueue_is_empty(input)) {
+    if (TokenQueue_is_empty(input))
+    {
         Error_throw_printf("Unexpected end of input (expected \'%s\')\n", text);
     }
-    Token* token = TokenQueue_remove(input);
-    if (token->type != type || !token_str_eq(token->text, text)) {
+    Token *token = TokenQueue_remove(input);
+    if (token->type != type || !token_str_eq(token->text, text))
+    {
         Error_throw_printf("Expected \'%s\' but found '%s' on line %d\n",
-                text, token->text, get_next_token_line(input));
+                           text, token->text, get_next_token_line(input));
     }
     Token_free(token);
 }
@@ -54,9 +57,10 @@ void match_and_discard_next_token (TokenQueue* input, TokenType type, const char
  * 
  * @param input Token queue to modify
  */
-void discard_next_token (TokenQueue* input)
+void discard_next_token(TokenQueue *input)
 {
-    if (TokenQueue_is_empty(input)) {
+    if (TokenQueue_is_empty(input))
+    {
         Error_throw_printf("Unexpected end of input\n");
     }
     Token_free(TokenQueue_remove(input));
@@ -69,12 +73,13 @@ void discard_next_token (TokenQueue* input)
  * @param type Expected type of next token
  * @returns True if the next token is of the expected type, false if not
  */
-bool check_next_token_type (TokenQueue* input, TokenType type)
+bool check_next_token_type(TokenQueue *input, TokenType type)
 {
-    if (TokenQueue_is_empty(input)) {
+    if (TokenQueue_is_empty(input))
+    {
         return false;
     }
-    Token* token = TokenQueue_peek(input);
+    Token *token = TokenQueue_peek(input);
     return (token->type == type);
 }
 
@@ -86,12 +91,13 @@ bool check_next_token_type (TokenQueue* input, TokenType type)
  * @param text Expected text of next token
  * @returns True if the next token is of the expected type and text, false if not
  */
-bool check_next_token (TokenQueue* input, TokenType type, const char* text)
+bool check_next_token(TokenQueue *input, TokenType type, const char *text)
 {
-    if (TokenQueue_is_empty(input)) {
+    if (TokenQueue_is_empty(input))
+    {
         return false;
     }
-    Token* token = TokenQueue_peek(input);
+    Token *token = TokenQueue_peek(input);
     return (token->type == type) && (token_str_eq(token->text, text));
 }
 
@@ -101,20 +107,28 @@ bool check_next_token (TokenQueue* input, TokenType type, const char* text)
  * @param input Token queue to modify
  * @returns Parsed type (it is also removed from the queue)
  */
-DecafType parse_type (TokenQueue* input)
+DecafType parse_type(TokenQueue *input)
 {
-    Token* token = TokenQueue_remove(input);
-    if (token->type != KEY) {
+    Token *token = TokenQueue_remove(input);
+    if (token->type != KEY)
+    {
         Error_throw_printf("Invalid type '%s' on line %d\n", token->text, get_next_token_line(input));
     }
     DecafType t = VOID;
-    if (token_str_eq("int", token->text)) {
+    if (token_str_eq("int", token->text))
+    {
         t = INT;
-    } else if (token_str_eq("bool", token->text)) {
+    }
+    else if (token_str_eq("bool", token->text))
+    {
         t = BOOL;
-    } else if (token_str_eq("void", token->text)) {
+    }
+    else if (token_str_eq("void", token->text))
+    {
         t = VOID;
-    } else {
+    }
+    else
+    {
         Error_throw_printf("Invalid type '%s' on line %d\n", token->text, get_next_token_line(input));
     }
     Token_free(token);
@@ -128,17 +142,18 @@ DecafType parse_type (TokenQueue* input)
  * @param buffer String buffer for parsed identifier (should be at least
  * @c MAX_TOKEN_LEN characters long)
  */
-void parse_id (TokenQueue* input, char* buffer)
+void parse_id(TokenQueue *input, char *buffer)
 {
-    Token* token = TokenQueue_remove(input);
-    if (token->type != ID) {
+    Token *token = TokenQueue_remove(input);
+    if (token->type != ID)
+    {
         Error_throw_printf("Invalid ID '%s' on line %d\n", token->text, get_next_token_line(input));
     }
     snprintf(buffer, MAX_ID_LEN, "%s", token->text);
     Token_free(token);
 }
 
-ASTNode* parse_vardecl (TokenQueue* input)
+ASTNode *parse_vardecl(TokenQueue *input)
 {
     /*
     *VarDecl -> Type
@@ -146,26 +161,96 @@ ASTNode* parse_vardecl (TokenQueue* input)
     DecafType t = parse_type(input);
     char buffer[MAX_TOKEN_LEN];
     parse_id(input, buffer);
+    int arrayLength = -1;
+    // If array...
+    char* first = TokenQueue_peek(input)->text;
+    if (token_str_eq("\\[", first))
+    {
+        // First bracket
+        match_and_discard_next_token(input, SYM, "[");
+
+        // Second Bracket
+        match_and_discard_next_token(input, SYM, "]");
+    }
     match_and_discard_next_token(input, SYM, ";");
+    if (arrayLength >= 0)
+    {
+        return VarDeclNode_new(buffer, t, true, arrayLength, 1);
+    }
     return VarDeclNode_new(buffer, t, false, 0, 1);
+}
+
+ASTNode *parse_statement(TokenQueue *input, bool* isValid)
+{
+    Token* first = TokenQueue_peek(input);
+    if (token_str_eq("break",first->text))
+    {
+        free(TokenQueue_remove(input));
+        match_and_discard_next_token(input, SYM, ";");
+        return BreakNode_new(1);
+    }
+        if (token_str_eq("continue",first->text))
+    {
+        free(TokenQueue_remove(input));
+        match_and_discard_next_token(input, SYM, ";");
+        return ContinueNode_new(1);
+    }
+    *isValid = false;
+    return NULL;
+}
+
+ASTNode *parse_block(TokenQueue *input)
+{
+    ASTNode *block = BlockNode_new(1);
+    match_and_discard_next_token(input, SYM, "{");
+    bool isValid = true;
+    // Statements
+    // While program still says valid
+    while (!TokenQueue_is_empty(input) && isValid)
+    {
+        parse_statement(input, &isValid);
+    }
+    match_and_discard_next_token(input, SYM, "}");
+    return block;
+}
+
+ASTNode *parse_funcdecl(TokenQueue *input)
+{
+    DecafType t = parse_type(input);
+    char buffer[MAX_TOKEN_LEN];
+    parse_id(input, buffer);
+    // RESTART HERE FOR PARAMETERS
+    match_and_discard_next_token(input, SYM, "(");
+    match_and_discard_next_token(input, SYM, ")");
+    ASTNode *block_node = parse_block(input);
+    return FuncDeclNode_new(buffer, t, NULL, block_node, 1);
 }
 
 /*
  * node-level parsing functions
  */
 
-ASTNode* parse_program (TokenQueue* input)
+ASTNode *parse_program(TokenQueue *input)
 {
-    ASTNode* node = ProgramNode_new();
+    ASTNode *node = ProgramNode_new();
 
     while (!TokenQueue_is_empty(input))
     {
-        NodeList_add(node->program.variables, parse_vardecl(input));
-    }    
+        if (token_str_eq("def", TokenQueue_peek(input)->text))
+        {
+            Token *token = TokenQueue_remove(input);
+            free(token);
+            NodeList_add(node->program.variables, parse_funcdecl(input));
+        }
+        else
+        {
+            NodeList_add(node->program.variables, parse_vardecl(input));
+        }
+    }
     return node;
 }
 
-ASTNode* parse (TokenQueue* input)
+ASTNode *parse(TokenQueue *input)
 {
     return parse_program(input);
 }
